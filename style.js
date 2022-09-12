2450e197cfe0b5f6a48011e76bd4519ae580fc54
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-12 12:04:23 +0100

    added files

---
+++ style.js
@@ -0,0 +1,13 @@
+class Team {
+    constructor(playerName,position,jerseyNumber){
+        this.playerName = playerName,
+        this.position = position,
+        this.jerseyNumber = jerseyNumber,
+        this.club = club
+
+    }
+    
+}
+var player1 = new Team("Antony","forward","9")
+    
+
