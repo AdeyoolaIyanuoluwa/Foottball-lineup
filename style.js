2f791b6d7dfc5dc49f501791b596fa04ae33f64e
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-12 13:43:50 +0100

    did the styling

--- style.js
+++ style.js
@@ -6,11 +6,44 @@ class Team {
     }
     
 }
-var player1 = new Team("Antony","forward","9")
+var player1 = new Team("Antony","FORWARD","9")
 console.log (player1)
     ninth.innerHTML = `
-   <p><img src="j-removebg-preview.png" alt=""><span cla>${player1.jerseyNumber}</span><p>
-        <h6>${player1.playerName}</h6>
-        <h6>${player1.position}</h4>
-    `
+   <p>
+        <img src="j-removebg-preview.png" alt="">
+        <span class = "number">${player1.jerseyNumber}</span>
+        <span class = "name">${player1.playerName}</span>
+        <span class = "position">${player1.position}</span>
+   <p>
+   `
+var player2 = new Team("Rashford","FORWARD","10")
+console.log (player2)
+    tenth.innerHTML = `
+   <p>
+        <img src="j-removebg-preview.png" alt="">
+        <span class = "number">${player2.jerseyNumber}</span>
+        <span class = "name">${player2.playerName}</span>
+        <span class = "position">${player2.position}</span>
+   <p>
+   `
+var player3 = new Team("Ronaldo","FORWARD","5")
+console.log (player3)
+eleventh.innerHTML = `
+   <p>
+        <img src="j-removebg-preview.png" alt="">
+        <span class = "number">${player3.jerseyNumber}</span>
+        <span class = "name">${player3.playerName}</span>
+        <span class = "position">${player3.position}</span>
+   <p>
+   `
+var player4 = new Team("Ronaldo","FORWARD","5")
+console.log (player4)
+sixth.innerHTML = `
+   <p>
+        <img src="j-removebg-preview.png" alt="">
+        <span class = "number">${player4.jerseyNumber}</span>
+        <span class = "name">${player4.playerName}</span>
+        <span class = "position">${player4.position}</span>
+   <p>
+   `
 
