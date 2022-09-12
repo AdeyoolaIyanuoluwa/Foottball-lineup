69ff7d01033878c647e33a3a475e9183acbdc63e
Adeyoola Iyanuoluwa <adeyoolaiyanuoluwa@gmail.com>
2022-09-12 12:31:21 +0100

    added background image

--- style.js
+++ style.js
@@ -2,12 +2,15 @@ class Team {
     constructor(playerName,position,jerseyNumber){
         this.playerName = playerName,
         this.position = position,
-        this.jerseyNumber = jerseyNumber,
-        this.club = club
-
+        this.jerseyNumber = jerseyNumber
     }
     
 }
 var player1 = new Team("Antony","forward","9")
-    
+console.log (player1)
+    ninth.innerHTML = `
+   <p><img src="j-removebg-preview.png" alt=""><span cla>${player1.jerseyNumber}</span><p>
+        <h6>${player1.playerName}</h6>
+        <h6>${player1.position}</h4>
+    `
 
